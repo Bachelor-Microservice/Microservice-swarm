32f16ae855bed5de549a022e282a325a88fa5774
Casper Hansern <caspha17@gmail.com>
2020-03-17 14:27:48 +0100

    Added compose

--- example_worker/Controllers/WeatherForecastController.cs
+++ example_worker/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -24,16 +25,17 @@ namespace example_worker.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+           HttpClient client = new HttpClient();
+
+        
+
+           var response = await client.GetAsync(new Uri("http://worker2:5001/WeatherForecast"));
+
+           System.Console.WriteLine(response);
+
+
         }
     }
 }

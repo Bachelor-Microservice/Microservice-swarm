d7d89815a340a9211ce1c043f23967a7c0a3693b
Casper Hansern <caspha17@gmail.com>
2020-03-22 20:17:56 +0100

    Added new feature

--- example-worker2/Controllers/WeatherForecastController.cs
+++ example-worker2/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace example_worker2.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 20).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),

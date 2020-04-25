8e63983e175a1ef47706836d688493ccff048e77
Oskar Kristiansen <skakkristiansen@gmail.com>
2020-04-25 13:59:20 +0200

    Added swagger

--- PriceCalendarService/Startup.cs
+++ PriceCalendarService/Startup.cs
@@ -10,7 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PriceCalendarService.Models;
+using PriceCalendarService.Services;
+using AutoMapper;
 
 namespace PriceCalendarService
 {
@@ -28,16 +31,27 @@ namespace PriceCalendarService
         {
             services.AddControllers();
             services.AddTransient<PriceCalendarServiceContext>();
+            services.AddScoped<IItemPriceAndCurrencyResponseService, ItemPriceAndCurrencyResponseService>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "PriceCalendarService" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PriceCalendarService");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
 
             app.UseCors( e=> e.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();

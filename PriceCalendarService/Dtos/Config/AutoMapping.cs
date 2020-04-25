36b1912ef586a0b6063fdda8648a410301e45af5
Oskar Kristiansen <skakkristiansen@gmail.com>
2020-04-25 13:59:01 +0200

    Added DTO's + Automapping

---
+++ PriceCalendarService/Dtos/Config/AutoMapping.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Routing.Constraints;
+using PriceCalendarService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCalendarService.Dtos.Config
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            /*CreateMap<ItemPriceAndCurrencyResponseDTO, ItemPriceAndCurrencyResponse>();
+            CreateMap<ItemPriceAndCurrencyResponse, ItemPriceAndCurrencyResponseDTO>();
+            */
+            CreateMap<ItemDayDTO, ItemDay>();
+            CreateMap<ItemDay, ItemDayDTO>();
+
+            CreateMap<ItemDTO, Item>()
+                .ForMember(i => i.ItemDay, o => o.MapFrom(src => src.ItemDays)).ReverseMap();
+
+            CreateMap<GroupsDTO, Groups>()
+                .ForMember(g => g.Item, o => o.MapFrom(src => src.Items)).ReverseMap();
+
+            CreateMap<ItemPriceAndCurrencyResponseDTO, ItemPriceAndCurrencyResponse>()
+                .ForMember(d => d.Groups, o => o.MapFrom(src => src.Groups));
+        }
+    }
+}

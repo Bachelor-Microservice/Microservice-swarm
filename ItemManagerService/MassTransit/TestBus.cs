44dd3f6715ffdc7317013c1e4560ca1097b97f33
Oskar Kristiansen <skakkristiansen@gmail.com>
2020-05-03 01:34:04 +0200

    Messaging Contracts added (temp local - convert to nugget if time)

---
+++ ItemManagerService/MassTransit/TestBus.cs
@@ -0,0 +1,43 @@
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MessageLibrary;
+
+namespace ItemManagerService.MassTransit
+{
+   public class TestBus
+    {
+        public static async Task Test()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host("rabbitmq");
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Consumer<MyMessageConsumer>();
+                    ep.Handler<Message>(context =>
+                    {
+                        
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync(); // This is important!
+
+            await bus.Publish(new Message { Text = "FROM_PUBLISHER" });
+        }
+
+    }
+
+    public class MyMessageConsumer : IConsumer<Message>
+    {
+        public async Task Consume(ConsumeContext<Message> context)
+        {
+            Console.WriteLine("CONSUMING FROM FUNC");
+        }
+    }
+}
---
+++ PriceCalendarService/MassTransit/Contracts/ItemEntityDeleted.cs
@@ -0,0 +1,7 @@
+namespace Shared.MassTransit.Contracts
+{
+    public class ItemEntityDeleted
+    {
+        public int Id { get; set; }
+    }
+}

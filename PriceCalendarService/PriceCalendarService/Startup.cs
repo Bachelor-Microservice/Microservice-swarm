46de55f5f9350d876a0f581be76498b04043d4db
Oskar Kristiansen <skakkristiansen@gmail.com>
2020-05-17 12:11:13 +0200

    App indsight added to pricecalendar

--- PriceCalendarService/PriceCalendarService/Startup.cs
+++ PriceCalendarService/PriceCalendarService/Startup.cs
@@ -67,6 +67,7 @@ namespace PriceCalendarService
                 }));
 
         MassTransit.Config.InitiateAndInject.ConnectToQueue(services);
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
